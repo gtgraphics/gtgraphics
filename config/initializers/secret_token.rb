a08a937e7fa5ed2c61c58f6c127d3af83f72115c
Tobias Casper <tobias.casper@gmail.com>
2013-06-11 21:24:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GTGraphics::Application.config.secret_key_base = 'c2f4af26f03da9f2eb64d361842c76db48c1f346e0900701cab73dcf4e66111138029db6422dcf6bcc980f9aecc96824c7caa2fc4ce320466eea0a15a9adf295'
