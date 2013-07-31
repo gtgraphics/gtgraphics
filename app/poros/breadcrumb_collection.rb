9a346f21a4c8cea97da8db82e9d359a32ecd6468
Tobias Casper <tobias.casper@gmail.com>
2013-07-31 21:37:47 +0200

    updates breadcrumbs

--- app/poros/breadcrumb_collection.rb
+++ app/poros/breadcrumb_collection.rb
@@ -22,46 +22,4 @@ class BreadcrumbCollection < Array
     substr = map { |item| "\"#{item.caption}\"=>\"#{item.path}\"" }.join(', ')
     "#<#{self.class.name} #{substr}>"
   end
-
-=begin
-  def collection(collection_name_or_class, options = {})
-    options = options.reverse_merge(default_options)
-    options.assert_valid_keys(:only, :except, :namespace)
-
-    namespace = options.delete(:namespace)
-    klass = case collection_name_or_class
-    when Symbol, String
-      collection_name_or_class.to_s.classify.constantize
-    else
-      collection_name_or_class
-    end
-    caption = klass.model_name.human(count: 2)
-    add(caption, [namespace, klass.model_name.collection].flatten(1).compact, options)
-  end
-
-  def element(record_name_or_object_or_class, options = {})
-    options = options.reverse_merge(default_options)
-    options.assert_valid_keys(:only, :except, :namespace)
-
-    namespace = options.delete(:namespace)
-    object = case record_name_or_object_or_class
-    when Symbol, String
-      controller_context.instance_variable_get("@#{record_name_or_object_or_class}")
-    when Class
-      controller_context.instance_variable_get("@#{record_name_or_object_or_class.model_name.element}")
-    else
-      record_name_or_object_or_class
-    end
-    caption = CAPTION_LOOKUP_ORDER.collect { |method| object.try(method) }.compact.first
-    add(caption, [namespace, object].flatten(1).compact, options)
-  end
-
-  def resource(resource_name, options = {})
-    element_options = options.dup
-    element_options[:only] = %i(show edit update) & Array(options[:only]).map(&:to_sym) if options[:only]
-
-    collection(resource_name, options)
-    element(resource_name, element_options)
-  end
-=end
 end
