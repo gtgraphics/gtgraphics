49a4c7494c4eca618611215b00fbde35cd23bb77
Tobias Casper <tobias.casper@gmail.com>
2013-12-22 23:30:44 +0100

    fixes several bugs

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ GtGraphics::Application.routes.draw do
         end
 
         resources :images do
+          get 'by/:sort/:direction', action: :index, as: :order
           resources :styles, controller: :image_styles
           collection do
             patch :batch, as: :batch_process
