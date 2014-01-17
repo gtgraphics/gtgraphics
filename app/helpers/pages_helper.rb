3f2835a094a3d8a19f20425f2502ec2231b694f8
Tobias Casper <tobias.casper@gmail.com>
2014-01-17 22:51:50 +0100

    renames bang method in Page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,6 @@
 module PagesHelper
   def render_region(name)
+    @page.region_labels.include?(name.to_s)
     content_tag :div, class: 'region', data: { region: name } do
       #(@page.contents || {}).fetch(name, '').html_safe
     end
