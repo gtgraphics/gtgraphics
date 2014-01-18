eb94c50e21998a257dfef279d234fa91dee306d4
Tobias Casper <tobias.casper@gmail.com>
2014-01-18 02:12:44 +0100

    adds some test regions to homepage template

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,14 +3,14 @@ module PagesHelper
     template = Liquid::Template.parse(content)
     template.render(@page.to_liquid).html_safe
   end
-  
+
   def render_region(name)
     if region_definition = @page.template.region_definitions.find_by(label: name)
-      if region = @page.regions.find_by(definition_id: region_definition.id)
-        if can? :update, @page
-          data = { region: name, url: admin_page_path(@page), method: :patch }        
-        end
-        content_tag :div, class: 'region well', data: data do
+      if can? :update, @page
+        data = { region: name, url: admin_page_path(@page), method: :patch }        
+      end
+      content_tag :div, class: 'region well', data: data do
+        if region = @page.regions.find_by(definition_id: region_definition.id)
           render_content(region.body)
         end
       end
