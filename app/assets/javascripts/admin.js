5d112b94ff55af761616d0aefb0c64eb43232b64
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:32:14 +0100

    only removes shared

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,8 +1,8 @@
 //= require_self
 // require shared
-// require admin/table_checkbox
-// require admin/cropper
-// require admin/translation_manager
-// require admin/page_translation_manager
-// require admin/page_embeddable_manager
-// require_tree ./admin/views
+//= require admin/table_checkbox
+//= require admin/cropper
+//= require admin/translation_manager
+//= require admin/page_translation_manager
+//= require admin/page_embeddable_manager
+//= require_tree ./admin/views
