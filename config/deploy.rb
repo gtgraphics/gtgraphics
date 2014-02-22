d16b922e1d209c7f472a968869f6f5a8d547553e
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:28:33 +0100

    bla

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,9 @@ set :rvm_ruby_version, '2.0.0'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
+
+set :assets_roles, [:web, :app]
 
 # Default value for :linked_files is []
 set :linked_files, %w(config/database.yml)
