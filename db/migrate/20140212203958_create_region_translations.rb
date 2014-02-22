43e388e632ea8b8e84c39275ab027ef76bb90abf
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 15:24:46 +0100

    updates migration

--- db/migrate/20140212203958_create_region_translations.rb
+++ db/migrate/20140212203958_create_region_translations.rb
@@ -1,15 +1,12 @@
 class CreateRegionTranslations < ActiveRecord::Migration
   def up
-    Page::Region.create_translation_table! body: :text
-
-    rename_table :region_definitions, :template_region_definitions
     remove_column :regions, :concrete_region_id
     remove_column :regions, :concrete_region_type
     rename_table :regions, :page_regions
+    
+    Page::Region.create_translation_table! body: :text
 
-
-    drop_table :region_contents
-    drop_table :region_content_translations
+    rename_table :region_definitions, :template_region_definitions
   end
 
   def down
