21ee7fc9407c03edd0b88f0d42b9af07639898eb
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:29:14 +0100

    removes further assts

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,7 +2,7 @@
 //= require shared
 //= require admin/table_checkbox
 // require admin/cropper
-//= require admin/translation_manager
-//= require admin/page_translation_manager
-//= require admin/page_embeddable_manager
+// require admin/translation_manager
+// require admin/page_translation_manager
+// require admin/page_embeddable_manager
 // require_tree ./admin/views
