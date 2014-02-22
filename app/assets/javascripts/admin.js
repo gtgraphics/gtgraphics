396dd6ef311f0490265c5fb5d41b6c39ab942574
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 21:25:20 +0100

    removes jquery

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,6 @@
 // require underscore
-//= require jquery
-//= require jquery_ujs
+// require jquery
+// require jquery_ujs
 // require_tree ./core_ext
 //= require turbolinks
 // require jquery.turbolinks
