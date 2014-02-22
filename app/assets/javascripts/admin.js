a1e51562773e454faca3b0f179cb3fd3b77b7202
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:30:22 +0100

    bla

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,6 @@
 //= require_self
 //= require shared
-//= require admin/table_checkbox
+// require admin/table_checkbox
 // require admin/cropper
 // require admin/translation_manager
 // require admin/page_translation_manager
