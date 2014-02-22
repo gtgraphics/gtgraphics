0c878fb17a236c0449dc51239df77e60ef1f8b87
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 21:41:45 +0100

    enables JS compression in production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,6 +29,7 @@ GtGraphics::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
