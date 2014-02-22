d0294e4de4e90e96ee4792d856f232c2d71bf3da
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 21:20:48 +0100

    only admin.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,2 @@
-// require underscore
-//= require jquery
-//= require jquery_ujs
-//= require_tree ./core_ext
-//= require turbolinks
-//= require jquery.turbolinks
-//= require bootstrap
-//= require_tree ./libraries
-//= require_tree ./shared
-// require ./editor
-
-//= require_tree ./application
+// require shared
+// require_tree ./application
