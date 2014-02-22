282c10e46a078e6213174f898a13988dc22c9e68
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 15:16:59 +0100

    removes table from unique indexes adding migration

--- db/migrate/20140111123030_add_unique_indexes_to_translations.rb
+++ db/migrate/20140111123030_add_unique_indexes_to_translations.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexesToTranslations < ActiveRecord::Migration
   def change
-    %w(attachments images pages project_pages region_contents snippets templates).each do |table_name|
+    %w(attachments images pages project_pages snippets templates).each do |table_name|
       translation_table_name = "#{table_name.singularize}_translations"
       foreign_key = "#{table_name.singularize}_id".to_sym
       index_name = "index_#{translation_table_name}_on_foreign_id_and_locale"
