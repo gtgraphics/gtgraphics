0f3b1f96a14e7f536d293e0323c07d41bf6df8ab
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:34:29 +0100

    refactors

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,5 +1,20 @@
 //= require_self
 // require shared
+
+//= require underscore
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery.turbolinks
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
+//= require bootstrap
+//= require_tree ./core_ext
+//= require_tree ./libraries
+//= require_tree ./shared
+//= require ./editor
+
+
 //= require admin/table_checkbox
 //= require admin/cropper
 //= require admin/translation_manager
