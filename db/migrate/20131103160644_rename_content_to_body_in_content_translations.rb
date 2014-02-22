57bb462a4fa54772f01f7b6b76369544b607985b
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 15:11:10 +0100

    removes old migrations

--- db/migrate/20131103160644_rename_content_to_body_in_content_translations.rb
+++
@@ -1,6 +0,0 @@
-class RenameContentToBodyInContentTranslations < ActiveRecord::Migration
-  def change
-    # rename_column :content_translations, :content, :body
-    rename_column :regions, :content, :body
-  end
-end
--- db/migrate/20131211185836_drop_region_translations.rb
+++
@@ -1,8 +0,0 @@
-class DropRegionTranslations < ActiveRecord::Migration
-  def up
-    drop_table :region_translations rescue nil
-  end
-
-  def down
-  end
-end
