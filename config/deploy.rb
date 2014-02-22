e1aab89c998da088fc2f3f3f8c56d11f10a6b2a2
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 16:47:44 +0100

    improves deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,13 @@ set :ssh_options, { forward_agent: true }
 
 # RVM
 # set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-set :rvm_type, :global
+# set :rvm_type, :global
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, lambda { "/home/gtgraphics/#{stage}" }
+ask :deploy_to, -> { "home/gtgraphics/#{rails_env}" }
 
 # Default value for :scm is :git
 # set :scm, :git
