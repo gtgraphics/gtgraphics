ef3bba42392fcf0a25940676368af7e4f41435c8
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 15:22:05 +0100

    removes one more table from cleanup migration

--- db/migrate/20140114225411_cleanup_unused_tables.rb
+++ db/migrate/20140114225411_cleanup_unused_tables.rb
@@ -1,6 +1,6 @@
 class CleanupUnusedTables < ActiveRecord::Migration
   def up
-    %w(contact_forms contents page_templates project_translations redirections shouts).each do |table_name|
+    %w(contact_forms contents page_templates redirections shouts).each do |table_name|
       drop_table table_name
     end
   end
