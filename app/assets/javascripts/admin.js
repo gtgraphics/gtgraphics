2daeda56ad0be568b82f74d5e2bfbe9665e18f1f
Tobias Casper <tobias.casper@gmail.com>
2014-02-22 20:38:27 +0100

    removes bootstrap

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -8,7 +8,7 @@
 //= require jquery.turbolinks
 //= require jquery.ui.draggable
 //= require jquery.ui.droppable
-//= require bootstrap
+// require bootstrap
 //= require_tree ./core_ext
 //= require_tree ./libraries
 // require_tree ./shared
