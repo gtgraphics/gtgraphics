a1fa38105fbe5ac085b93d7e5cc3485b8c63d859
Tobias Casper <tobias.casper@gmail.com>
2014-03-09 22:03:52 +0100

    updates Excludable module

--- app/models/concerns/excludable.rb
+++ app/models/concerns/excludable.rb
@@ -2,8 +2,8 @@ module Excludable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def without(records)
-      record_ids = Array(records).select(&:persisted?).map(&:id)
+    def without(*records)
+      record_ids = records.flatten.select(&:persisted?).map(&:id)
       if record_ids.any?
         where.not(id: record_ids.many? ? record_ids : record_ids.first)
       else
