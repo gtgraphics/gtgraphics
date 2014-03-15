9e08e3570dc046d54f09ed45f885da1aaa2b1e18
Tobias Casper <tobias.casper@gmail.com>
2014-03-15 01:22:30 +0100

    fixes date validator

--- app/validators/localized_date_validator.rb
+++ app/validators/localized_date_validator.rb
@@ -4,16 +4,8 @@ class LocalizedDateValidator < ActiveModel::EachValidator
   end
 
   def validate_each(record, attribute, value)
-
     if value.is_a?(LocalizedDate) and !value.valid?
       record.errors.add(attribute, options[:message])
-    end
-
-    schemes = options.fetch(:schemes)
-    schemes = Array(schemes).map(&:to_s) unless schemes == :all
-    uri = URI.parse(value)
-    if uri.host.nil? or !uri.scheme.in?(schemes)
-      record.errors.add(attribute, options.fetch(:message), url: value)
     end
   end
 end
