c29fd023bfce2f899eef6a0960ac47c396ebe2d9
Tobias Casper <tobias.casper@gmail.com>
2014-04-28 21:55:29 +0200

    Refactors IconHelper, removes LegacyPageTreePresenter

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -39,41 +39,12 @@ module IconHelper
     content_tag :i, nil, class: icon_class, style: options[:style]
   end
 
-  def append_icon(name, *args, &block)
-    options = args.extract_options!
-    caption = args.first
-    caption ||= capture(&block) if block_given?
-    raise ArgumentError, 'no text supplied' if caption.nil?
-
-    caption_options = options.delete(:caption_html) { Hash.new }
-    caption_options[:class] ||= ''
-    caption_options[:class] << ' caption'
-    caption_options[:class].strip!
-
-    content_tag :span, class: 'append-icon' do
-      html = ""
-      html << content_tag(:span, caption, caption_options) if caption.present?
-      html << icon(name, options)
-      html.html_safe
-    end
+  def append_icon(*args, &block)
+    _labelled_icon(:append, *args, &block)
   end
 
-  def prepend_icon(name, *args, &block)
-    options = args.extract_options!
-    caption = args.first
-    caption ||= capture(&block) if block_given?
-    raise ArgumentError, 'no text supplied' if caption.nil?
-
-    caption_options = options.delete(:caption_html) { Hash.new }
-    caption_options[:class] ||= ''
-    caption_options[:class] << ' caption'
-    caption_options[:class].strip!
-
-    content_tag :span, class: 'prepend-icon' do
-      html = icon(name, options)
-      html << content_tag(:span, caption, caption_options) if caption.present?
-      html.html_safe
-    end
+  def prepend_icon(*args, &block)
+    _labelled_icon(:prepend, *args, &block)
   end
 
   # Flags
@@ -87,7 +58,16 @@ module IconHelper
     image_tag("flags/#{size}/#{locale}.png", options)
   end
 
-  def append_flag_icon(locale, *args, &block)
+  def append_flag_icon(*args, &block)
+    _labelled_flag_icon(:append, *args, &block)
+  end
+
+  def prepend_flag_icon(*args, &block)
+    _labelled_flag_icon(:prepend, *args, &block)
+  end
+
+  private
+  def _labelled_icon(location, name, *args, &block)
     options = args.extract_options!
     caption = args.first
     caption ||= capture(&block) if block_given?
@@ -98,15 +78,14 @@ module IconHelper
     caption_options[:class] << ' caption'
     caption_options[:class].strip!
 
-    content_tag :span, class: 'append-flag-icon' do
-      html = ""
-      html << content_tag(:span, caption, caption_options) if caption.present?
-      html << flag_icon(locale, options)
-      html.html_safe
+    content_tag :span, class: "#{location}-icon" do
+      concat icon(name, options) if location == :prepend
+      concat content_tag(:span, caption, caption_options) if caption.present?
+      concat icon(name, options) if location == :append
     end
   end
 
-  def prepend_flag_icon(locale, *args, &block)
+  def _labelled_flag_icon(location, locale, *args, &block)
     options = args.extract_options!
     caption = args.first
     caption ||= capture(&block) if block_given?
@@ -117,10 +96,10 @@ module IconHelper
     caption_options[:class] << ' caption'
     caption_options[:class].strip!
 
-    content_tag :span, class: 'prepend-flag-icon' do
-      html = flag_icon(locale, options)
-      html << content_tag(:span, caption, caption_options) if caption.present?
-      html.html_safe
+    content_tag :span, class: "#{location}-flag-icon" do
+      concat flag_icon(locale, options) if location == :prepend
+      concat content_tag(:span, caption, caption_options) if caption.present?
+      concat flag_icon(locale, options) if location == :append
     end
   end
 end
