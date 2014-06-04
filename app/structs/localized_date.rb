d0f6a62f3d80e014d27b3d7f9db8d48a413c2b02
Tobias Casper <tobias.casper@gmail.com>
2014-06-04 16:45:26 +0200

    Update localized_date.rb

--- app/structs/localized_date.rb
+++ app/structs/localized_date.rb
@@ -1,20 +1,20 @@
 # composed_of :released_on, class_name: 'LocalizedDate', mapping: %w(released_on to_s), constructor: :parse, converter: :parse
 
-class LocalizedDate < Struct.new(:date, :format, :locale)
+class LocalizedDate < Struct.new(:representation, :format, :locale)
   DEFAULT_FORMAT = :default
 
   def initialize(*args)
-    date, format, locale = args
+    representation, format, locale = args
     format ||= DEFAULT_FORMAT
     locale ||= I18n.locale
-    super(date, format, locale)
+    super(representation, format, locale)
   end
 
   def self.parse(date, options = {})
     format = options[:format]
     locale = options[:locale]
 
-    date_representation = case date
+    representation = case date
     when String
       date
     when Date
@@ -27,11 +27,11 @@ class LocalizedDate < Struct.new(:date, :format, :locale)
       raise ArgumentError, "unable to convert given #{date.class.name} to #{self.class.name}"
     end
 
-    new(date_representation, format, locale)
+    new(representation, format, locale)
   end
 
   def inspect
-    "#<#{self.class.name} date: #{date.inspect}, format: #{format.inspect}, locale: #{locale.inspect}>"
+    "#<#{self.class.name} representation: #{representation.inspect}, format: #{format.inspect}, locale: #{locale.inspect}>"
   end
 
   def resolved_format
@@ -43,16 +43,16 @@ class LocalizedDate < Struct.new(:date, :format, :locale)
   end
 
   def to_date
-    Date.strptime(date, resolved_format)
+    Date.strptime(representation, resolved_format)
   end
 
   def to_s
-    date
+    representation
   end
 
   def valid_date?
     begin
-      Date.strptime(date, resolved_format)
+      Date.strptime(representation, resolved_format)
       true
     rescue
       false
