86f843ee4466626cb005d47a6c46890d53b5ab6c
Tobias Casper <tobias.casper@gmail.com>
2014-06-04 16:42:39 +0200

    Update localized_date.rb

--- app/structs/localized_date.rb
+++ app/structs/localized_date.rb
@@ -1,47 +1,58 @@
-# composed_of :released_on, class_name: 'LocalizedDate', mapping: %w(released_on to_s), converter: :new
+# composed_of :released_on, class_name: 'LocalizedDate', mapping: %w(released_on to_s), constructor: :parse, converter: :parse
 
-class LocalizedDate < String
+class LocalizedDate < Struct.new(:date, :format, :locale)
   DEFAULT_FORMAT = :default
 
-  attr_reader :locale
+  def initialize(*args)
+    date, format, locale = args
+    format ||= DEFAULT_FORMAT
+    locale ||= I18n.locale
+    super(date, format, locale)
+  end
 
-  def initialize(date, options = {})
-    @format = options.fetch(:format, DEFAULT_FORMAT)
-    @locale = options.fetch(:locale, I18n.locale)
-    representation = case date
+  def self.parse(date, options = {})
+    format = options[:format]
+    locale = options[:locale]
+
+    date_representation = case date
     when String
       date
     when Date
-      if @format.is_a?(Symbol)
-        date = I18n.localize(date, format: @format, locale: @locale)
+      if format.is_a?(Symbol)
+        date = I18n.localize(date, format: format, locale: locale)
       else
-        date = date.strftime(@format)
+        date = date.strftime(format)
       end
     else
       raise ArgumentError, "unable to convert given #{date.class.name} to #{self.class.name}"
     end
-    super(representation)
-  end
 
-  def format
-    if @format.is_a?(Symbol)
-      I18n.translate(@format, scope: 'date.formats', locale: @locale) 
-    else
-      @format
-    end
+    new(date_representation, format, locale)
   end
 
   def inspect
-    "#<#{self.class.name} #{super}, format: #{@format.inspect}, locale: #{@locale.inspect}>"
+    "#<#{self.class.name} date: #{date.inspect}, format: #{format.inspect}, locale: #{locale.inspect}>"
+  end
+
+  def resolved_format
+    if format.is_a?(Symbol)
+      I18n.translate(format, scope: 'date.formats', locale: locale) 
+    else
+      format
+    end
   end
 
   def to_date
-    Date.strptime(self, format)
+    Date.strptime(date, resolved_format)
+  end
+
+  def to_s
+    date
   end
 
   def valid_date?
     begin
-      Date.strptime(self, format)
+      Date.strptime(date, resolved_format)
       true
     rescue
       false
