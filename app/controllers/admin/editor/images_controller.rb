6327a6291352cfc7f01e56549ee8724d4029fc12
Tobias Casper <tobias.casper@gmail.com>
2014-06-24 01:44:25 +0200

    fixes editor modal error (constant Admin::Editor::DialogProvidingController not found)

--- app/controllers/admin/editor/images_controller.rb
+++ app/controllers/admin/editor/images_controller.rb
@@ -1,5 +1,3 @@
 class Admin::Editor::ImagesController < Admin::Editor::ApplicationController
-  include Admin::Editor::DialogProvidingController
-
   editor_actions_for :image, params: [:external, :url, :image_id, :original_style, :style, :alternative_text, :width, :height, :alignment]
 end
--- app/controllers/admin/editor/links_controller.rb
+++ app/controllers/admin/editor/links_controller.rb
@@ -1,5 +1,3 @@
 class Admin::Editor::LinksController < Admin::Editor::ApplicationController
-  include Admin::Editor::DialogProvidingController
-  
   editor_actions_for :link, params: [:content, :external, :url, :page_id, :locale, :target]
 end
