09d9643ca4a60c9f192d2a35e1df78077c61b7ae
Tobias Casper <tobias.casper@gmail.com>
2014-07-23 23:44:48 +0200

    truncates image table when migrating to asset token usage

--- db/migrate/20140627214331_add_asset_token_to_images.rb
+++ db/migrate/20140627214331_add_asset_token_to_images.rb
@@ -1,6 +1,12 @@
 class AddAssetTokenToImages < ActiveRecord::Migration
-  def change
+  def up
+    Image.destroy_all
+
     add_column :images, :asset_token, :string, null: false
     add_index :images, :asset_token, unique: true
+  end
+
+  def down
+    remove_column :images, :asset_token
   end
 end
