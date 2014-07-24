1b704f28fd980269ff435810aa827517ab759053
Tobias Casper <tobias.casper@gmail.com>
2014-07-24 22:56:39 +0200

    refactors javascript structure

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,8 +1,4 @@
-//= require ./shared
 //= require jquery-fileupload/basic
-//= require admin/image_manipulator
-//= require admin/table_checkbox
-//= require admin/page_editor
+//= require ./shared
 //= require ./editor
-//= require ./admin/editor
-//= require_tree ./admin/views
+//= require_tree ./admin
