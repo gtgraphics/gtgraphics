675d08deab162c46209154caee9231aa5dac57d5
Tobias Casper <tobias.casper@gmail.com>
2014-08-10 16:40:05 +0200

    Fixed Shop Provider migration

--- db/migrate/20140805211826_add_shop_providers_to_image_pages.rb
+++ db/migrate/20140805211826_add_shop_providers_to_image_pages.rb
@@ -1,5 +1,30 @@
 class AddShopProvidersToImagePages < ActiveRecord::Migration
-  def change
+  def up
     add_column :image_pages, :shop_providers, :text
+
+    pages = select_all "SELECT id, metadata FROM pages"
+    pages.each do |page|
+      page_id = page['id']
+      metadata = YAML.load(page['metadata'])
+
+      new_metadata = {}
+      shop_providers = {}
+      metadata.each do |key, value|
+        if key =~ /\A(.*)_url\z/
+          shop_providers[$1] = value
+        else
+          new_metadata[key] = value
+        end
+      end
+      shop_providers = quote(shop_providers.to_yaml)
+      new_metadata = quote(new_metadata.to_yaml)
+
+      update "UPDATE pages SET metadata = #{new_metadata} WHERE id = #{page_id}"
+      update "UPDATE image_pages SET shop_providers = #{shop_providers} WHERE page_id = #{page_id}"
+    end
+  end
+
+  def down
+    remove_column :image_pages, :shop_providers
   end
 end
