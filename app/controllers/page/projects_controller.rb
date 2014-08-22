f8751698a01028aa3cf598b82efd645941074890
Tobias Casper <tobias.casper@gmail.com>
2014-08-23 00:40:29 +0200

    improves project view

--- app/controllers/page/projects_controller.rb
+++ app/controllers/page/projects_controller.rb
@@ -1,7 +1,14 @@
 class Page::ProjectsController < Page::ApplicationController
   before_action :load_project, only: :show
   
+  def default
+    @images = @project.images
+    respond_to do |format|
+      format.html { render_page }
+    end
+  end
+
   def load_project
     @project = @page.embeddable.project
-  end
+  end 
 end
