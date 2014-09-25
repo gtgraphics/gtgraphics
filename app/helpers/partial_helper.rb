caabfcdd685e9b8903ec40e3141de9ed25ad29f6
Tobias Casper <tobias.casper@gmail.com>
2014-09-25 14:25:09 +0200

    Update partial_helper.rb

--- app/helpers/partial_helper.rb
+++ app/helpers/partial_helper.rb
@@ -2,6 +2,12 @@ module PartialHelper
   def partial_exists?(path)
     path = path.to_s.split('/')
     partial_name = path.pop
+    path = path.join('/')
     lookup_context.template_exists?(partial_name, path, true)
+  end
+  
+  def try_render(*args)
+    render(*args)
+  rescue ActionView::MissingTemplate
   end
 end
