5a51ad1825056d21c8f64b64d03f227c755a5b09
Tobias Casper <tobias.casper@gmail.com>
2014-10-08 00:02:21 +0200

    improves background style and background image fetching

--- app/helpers/background_helper.rb
+++ app/helpers/background_helper.rb
@@ -1,9 +1,11 @@
 module BackgroundHelper
   def background_image
-    @background_image ||= Image.order('RANDOM()').first
+    @background_image ||= Image.joins(:pages).
+      where(pages: { id: Page.find_by(path: 'wallpapers').try(:self_and_descendants) }).
+      order('RANDOM()').first
   end 
 
   def background_url
-    attached_asset_path(background_image, :custom)
+    attached_asset_path(background_image, :custom) if background_image
   end 
 end
