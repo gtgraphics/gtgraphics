7cbfca81c08023b6ad0c53583fb77fe3865556b7
Tobias Casper <tobias.casper@gmail.com>
2014-11-29 18:55:04 +0100

    fixes tagging behavior

--- app/activities/admin/tag_assignment_activity.rb
+++ app/activities/admin/tag_assignment_activity.rb
@@ -5,10 +5,19 @@ class Admin::TagAssignmentActivity < Activity
 
   def perform
     records = record_type.constantize.where(id: record_ids)
+
+    common_tags = Tag.common(records).map(&:label)
+
     Tag.transaction do
       records.each do |record|
-        record.untag(tag_list.to_a - record.tag_list.to_a)
-        record.tag(tag_list.to_a)
+        persisted_tags = record.tag_list.to_a
+        added_tags = tag_list.to_a - persisted_tags
+        
+        # only common tags can be removed
+        removed_tags = (persisted_tags - tag_list.to_a) & common_tags
+
+        record.tag_list.add(added_tags)
+        record.tag_list.remove(removed_tags)
         record.save!
       end
     end
