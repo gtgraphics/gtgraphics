41a0ec3cc9aa6c2e02ac7fdaa6e804ed45e72982
Tobias Casper <tobias.casper@gmail.com>
2014-12-11 00:29:44 +0100

    fixes translations

--- app/helpers/loader_helper.rb
+++ app/helpers/loader_helper.rb
@@ -1,7 +1,7 @@
 module LoaderHelper
   def loader(options = {})
     content_tag :div, id: 'loader' do
-      prepend_icon :circle, translate('helpers.loader.loading'), outline: true, style: :notch, fixed_width: true, spin: true
+      prepend_icon :circle, translate('views.loader.loading'), outline: true, style: :notch, fixed_width: true, spin: true
     end
   end
 end
