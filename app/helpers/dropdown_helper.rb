8a822b7173c347da9cc53a47a91818cd8bbd2fee
Tobias Casper <tobias.casper@gmail.com>
2014-12-30 05:03:01 +0100

    improves menu

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -3,6 +3,7 @@ module DropdownHelper
     html_options = options.dup
 
     url = html_options.delete(:href) { '#' }
+    right = html_options.delete(:right) { false }
 
     html_options[:id] ||= "dropdown_#{microtimestamp}"
     html_options[:class] = ['dropdown-toggle', html_options[:class]].compact
@@ -10,6 +11,7 @@ module DropdownHelper
 
     menu_html_options = html_options.delete(:menu_html) { Hash.new }
     menu_html_options[:class] = ['dropdown-menu',
+                                 right ? 'dropdown-menu-right' : nil,
                                  menu_html_options[:class]].compact
     menu_html_options.merge!(role: 'menu',
                              'aria-labelledby' => html_options[:id])
