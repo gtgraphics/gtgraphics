e28dbcd9f90933027b7441c43cfb8c51bd96360b
Tobias Casper <tobias.casper@gmail.com>
2015-01-29 00:46:41 +0100

    fixes correct task name

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
     end
-    invoke 'delayed_job:restart'
+    invoke 'deploy:delayed_job:restart'
   end
 
   after :publishing, :restart
