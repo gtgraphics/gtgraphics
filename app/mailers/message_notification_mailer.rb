d5f3ca25090f71d6050622e37d3662cb3e96ff13
Tobias Casper <tobias.casper@gmail.com>
2015-01-29 01:01:51 +0100

    sets default subject on notification mailer

--- app/mailers/message_notification_mailer.rb
+++ app/mailers/message_notification_mailer.rb
@@ -2,16 +2,18 @@ class MessageNotificationMailer < ActionMailer::Base
   layout 'admin/mailer'
 
   helper :attached_asset
-  
+
   def notification_email(message, recipient)
     @message = message
     @recipient = recipient
 
     if message.is_a?(Message::BuyRequest)
-      subject = I18n.translate('views.admin.messages.buy_request_subject', image: message.image)
+      subject = translate('views.admin.messages.buy_request_subject',
+                          image: message.image).presence
     else
-      subject = @message.subject
+      subject = @message.subject.presence
     end
+    subject ||= translate('views.admin.messages.default_subject')
 
     mail to: @recipient.rfc5322, from: @message.sender, subject: subject
   end
