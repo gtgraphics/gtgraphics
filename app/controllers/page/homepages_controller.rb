29829e7981832aee0e6de670b74ec304d9584f3b
Tobias Casper <tobias.casper@gmail.com>
2015-01-29 21:36:23 +0100

    improves homepages controller

--- app/controllers/page/homepages_controller.rb
+++ app/controllers/page/homepages_controller.rb
@@ -1,6 +1,6 @@
 class Page::HomepagesController < Page::ApplicationController
   def show
-    @images = Image.tagged('Layout').limit(5).shuffle
+    @images = Image.tagged('Layout').shuffle.limit(5)
     respond_with_page
   end
 end
