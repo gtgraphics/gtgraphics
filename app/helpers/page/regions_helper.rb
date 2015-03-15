0daa5d299702f8eb412034bd6829c5bede68c6bc
Tobias Casper <tobias.casper@gmail.com>
2015-03-16 00:53:12 +0100

    improves regions helper

--- app/helpers/page/regions_helper.rb
+++ app/helpers/page/regions_helper.rb
@@ -12,20 +12,13 @@ module Page::RegionsHelper
   def yield_region(label)
     @page.check_template_support!
     definition = @region_definitions.find { |d| d.label == label.to_s }
-    if definition
-      region = @page.regions.find do |r|
-        r.definition_id == definition.id
-      end
-      cache [@page, region] do
-        body = region.body.html_safe
-        body = nil unless html_present?(body)
-        content_tag :div, body,
-                    id: "#{label}_region", class: [
-                      'region', "region-#{definition.region_type.dasherize}"]
-      end
-    elsif Rails.env.development?
-      fail Template::RegionDefinition::NotFound.new(label, @page.template)
-    end
+    return if definition.nil?
+    region = @page.regions.find { |r| r.definition_id == definition.id }
+    body = region.body.html_safe
+    body = nil unless html_present?(body)
+    content_tag :div, body,
+                id: "#{label}_region",
+                class: ['region', "region-#{definition.region_type.dasherize}"]
   end
 
   alias_method :render_region, :yield_region
