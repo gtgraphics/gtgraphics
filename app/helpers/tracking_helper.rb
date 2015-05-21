ae3d3bd11979f92ddd738aa6babd6e61bc179f96
Tobias Casper <tobias.casper@gmail.com>
2015-05-21 18:46:12 +0200

    adds Google Analytics tracking script

--- app/helpers/tracking_helper.rb
+++ app/helpers/tracking_helper.rb
@@ -2,6 +2,15 @@ module TrackingHelper
   def include_tracking_script
     return unless Rails.env.production?
     capture do
+      concat piwik_tracking_script
+      concat google_analytics_tracking_script
+    end
+  end
+
+  private
+
+  def piwik_tracking_script
+    capture do
       concat javascript_tag <<-JAVASCRIPT.strip_heredoc
         var _paq = _paq || [];
 
@@ -22,5 +31,17 @@ module TrackingHelper
       concat '<noscript><p><img src="http://stats.spdns.de/piwik.php?idsite=5" ' \
              'style="border:0;" alt="" /></p></noscript>'.html_safe
     end
+  end
+
+  def google_analytics_tracking_script
+    javascript_tag(<<-JAVASCRIPT.strip_heredoc)
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-47701772-2', 'auto');
+      ga('send', 'pageview');
+    JAVASCRIPT
   end
 end
