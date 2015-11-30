e790a8b931cf77d87537776dfdf3cb392ecf0383
Tobias Casper <tobias.casper@gmail.com>
2015-12-01 00:22:53 +0100

    fixes slash buster

--- lib/slash_buster.rb
+++ lib/slash_buster.rb
@@ -6,9 +6,15 @@ class SlashBuster
   end
 
   def call(env)
+    return @app.call(env) if %w(GET HEAD).exclude?(env['REQUEST_METHOD'])
+
     uri = URI.parse(env['REQUEST_URI'])
     return @app.call(env) unless uri.path.end_with?(SLASH)
+
     uri.path = uri.path.sub(/#{Regexp.escape(SLASH)}+\z/, '')
+
+    Rails.logger.info "Busting slashes and redirecting to: #{uri.to_s}"
+
     [301, { 'Location' => uri.to_s }, []]
   end
 end
