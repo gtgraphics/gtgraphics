0ff995d8f2946c632c7a981ce11fae4be95380c7
Tobias Casper <tobias.casper@gmail.com>
2016-01-25 00:19:44 +0100

    cleans up Page::ApplicationController

--- app/controllers/page/application_controller.rb
+++ app/controllers/page/application_controller.rb
@@ -1,85 +1,81 @@
-class Page::ApplicationController < ApplicationController
-  respond_to :html
+class Page
+  class ApplicationController < ::ApplicationController
+    respond_to :html
 
-  # before_action :restrict_secure_connection
+    before_action :load_page
+    before_action :load_template
+    before_action :track_hit
 
-  before_action :load_page
-  before_action :load_template
-  before_action :track_hit
+    attr_reader :page
+    protected :page
+    helper_method :page, :template_file
 
-  attr_reader :page
-  protected :page
-  helper_method :page, :template_file
-
-  breadcrumbs do |b|
-    pages = @page.self_and_ancestors.accessible_by(current_ability)
-            .with_translations_for_current_locale
-    pages.each do |page|
-      b.append page.title, page
+    breadcrumbs do |b|
+      pages = @page.self_and_ancestors.accessible_by(current_ability)
+              .with_translations_for_current_locale
+      pages.each do |page|
+        b.append page.title, page
+      end
     end
-  end
 
-  def show
-    respond_with_page
-  end
-
-  private
-
-  # Rendering
-
-  def render_page(options = {})
-    render template_path, options
-  end
-
-  def respond_with_page
-    respond_to do |format|
-      format.html { render_page }
-      yield(format) if block_given?
+    def show
+      respond_with_page
     end
-  end
 
-  def template_file
-    @page.check_template_support!
-    @template.filename.inquiry
-  end
+    private
 
-  def template_path
-    File.join(@page.embeddable_type.underscore.pluralize,
-              'templates', template_file)
-  end
+    # Rendering
 
-  # Filters
+    def render_page(options = {})
+      render template_path, options
+    end
 
-  def load_page
-    @page = current_page
-    return @page if can?(:read, @page)
-    fail ActionController::RoutingError, 'Page is invisible'
-  end
+    def respond_with_page
+      respond_to do |format|
+        format.html { render_page }
+        yield(format) if block_given?
+      end
+    end
 
-  def load_template
-    return unless current_page.support_templates?
-    @template = current_page.template
-    @region_definitions = @template.region_definitions
-  end
+    def template_file
+      @page.check_template_support!
+      @template.filename.inquiry
+    end
 
-  def load_menu_items
-    @menu_items = Page.primary.published.menu_items
-                  .accessible_by(current_ability)
-                  .with_translations_for_current_locale
-  end
+    def template_path
+      File.join(@page.embeddable_type.underscore.pluralize,
+                'templates', template_file)
+    end
 
-  def track_hit
-    @page.track_visit! if current_subroute.nil?
-  end
+    # Filters
 
-  # Helpers
+    def load_page
+      @page = current_page
+      return @page if can?(:read, @page)
+      fail ActionController::RoutingError, 'Page is invisible'
+    end
 
-  def localized_request_url(locale)
-    params = request.query_parameters.merge(locale: locale)
-    current_page_path(params)
-  end
+    def load_template
+      return unless current_page.support_templates?
+      @template = current_page.template
+      @region_definitions = @template.region_definitions
+    end
 
-  def restrict_secure_connection
-    fail ActionController::RoutingError, 'SSL not permitted' if request.ssl?
+    def load_menu_items
+      @menu_items = Page.primary.published.menu_items
+                    .accessible_by(current_ability)
+                    .with_translations_for_current_locale
+    end
+
+    def track_hit
+      @page.track_visit! if current_subroute.nil?
+    end
+
+    # Helpers
+
+    def localized_request_url(locale)
+      params = request.query_parameters.merge(locale: locale)
+      current_page_path(params)
+    end
   end
 end
