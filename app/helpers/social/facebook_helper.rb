f70598e3ce51234832e2f17adb5bb45abac9d6f1
Tobias Casper <tobias.casper@gmail.com>
2016-03-31 19:50:41 +0200

    improves facebook helper

--- app/helpers/social/facebook_helper.rb
+++ app/helpers/social/facebook_helper.rb
@@ -36,7 +36,7 @@ module Social::FacebookHelper
   def facebook_like_button(url, options = {})
     options = options.reverse_merge(
       width: '100%', style: :button_count, label: :like,
-      show_faces: true, share_button: false
+      show_faces: true, share_button: false, width: 200
     )
     content_tag :div, nil, class: 'fb-like', data: {
       href: url,
